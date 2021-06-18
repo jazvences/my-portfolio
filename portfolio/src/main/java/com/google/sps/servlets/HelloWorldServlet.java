e479680033f0b66997298232776f17656a8f2c3d
Jazmin Vences <jazvences@gmail.com>
2021-06-18 23:44:58 +0000

    Add Translation API, add Translation page, edit about me page

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -2,6 +2,7 @@ package com.google.sps.servlets;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,19 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-
-  public static ArrayList<String> msgs = new ArrayList<String>();
-
-  public static void main(String[] args) {
-    msgs.add("Have a good day!");
-    msgs.add("I am proud of you!");
-    msgs.add("You are amazing!");
-  }
   
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      //list of messages
+    final ArrayList<String> msgs = new ArrayList<>();
+    {
+        msgs.add("Have a good day!");
+        msgs.add("I am proud of you!");
+        msgs.add("You are amazing!");
+    }
+
     response.setContentType("text/html;");
-    response.getWriter().println("Hello User!");
     response.getWriter().println(msgs);
   }
   
